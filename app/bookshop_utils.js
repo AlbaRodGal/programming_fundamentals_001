fc8cab5ee12d5f03aa41c40023aa8090898cf2d7
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-02-12 20:45:16 +0000

    completed

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,7 +1,38 @@
 function addVAT(price) {
     return price + (0.2 * price);
 }
-
-module.exports = {
-    addVAT
+function getFullName(firstName, lastName){
+    return  firstName + " " + lastName
+}
+function makeHalfPrice(price){
+    return price*0.5
+}
+function countBooks(x){
+    return x.length;
 };
+function isInStock(book) {
+  if(book.quantity>=1)
+        return true;
+    else{
+        return false;
+    }
+};
+  function countBooks(x){
+    return x.length;
+};
+
+function getTotalOrderPrice(price, quantity) {
+  return price*quantity*0.20;
+  }
+module.exports = {
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice,
+    catalogueService.countBook;
+};
+
+
+
